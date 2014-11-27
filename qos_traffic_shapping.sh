c4cf6c04d1cd1601b820a43c207e593f31d8b6cb
Miguel Ángel Ajo <miguelangel@ajo.es>
2014-11-27 14:40:41 +0100

    Using veth pairs

--- qos_traffic_shapping.sh
+++ qos_traffic_shapping.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 QOS_BR=qos-test-br
 
-IF_A=test_interf0
+IF_A=test_interfA
+IF_A_2=test_interfA_ns
 IP_A=10.10.10.1
 NETNS_A=qos-netns-A
 
-IF_B=test_interf1
+IF_B=test_interfB
+IF_B_2=test_interfB_ns
 IP_B=10.10.10.2
 NETNS_B=qos-netns-B
 
@@ -13,23 +15,25 @@ NETMASK=24
 
 
 create_bridge_and_interfaces() {
-
 	ovs-vsctl add-br $QOS_BR
-	ovs-vsctl -- --may-exist add-port $QOS_BR $IF_A -- set Interface $IF_A type=internal
-	ovs-vsctl -- --may-exist add-port $QOS_BR $IF_B -- set Interface $IF_B type=internal
+	ip link add $IF_A type veth peer name $IF_A_2
+	ip link add $IF_B type veth peer name $IF_B_2
+	
+	ovs-vsctl add-port $QOS_BR $IF_A
+	ovs-vsctl add-port $QOS_BR $IF_B 
 }
 
 create_netns_and_set_interfaces() {
 
 	ip netns add $NETNS_A
 	ip netns add $NETNS_B
-	ip link set $IF_A netns $NETNS_A
-	ip link set $IF_B netns $NETNS_B
-	ip netns exec $NETNS_A ip addr add $IP_A/$NETMASK dev $IF_A
-	ip netns exec $NETNS_B ip addr add $IP_B/$NETMASK dev $IF_B
+	ip link set $IF_A_2 netns $NETNS_A
+	ip link set $IF_B_2 netns $NETNS_B
+	ip netns exec $NETNS_A ip addr add $IP_A/$NETMASK dev $IF_A_2
+	ip netns exec $NETNS_B ip addr add $IP_B/$NETMASK dev $IF_B_2
 	
-	ip netns exec $NETNS_A ip link set $IF_A up
-	ip netns exec $NETNS_B ip link set $IF_B up
+	ip netns exec $NETNS_A ip link set $IF_A_2 up
+	ip netns exec $NETNS_B ip link set $IF_B_2 up
 	#ip netns exec $NETNS_A ping -c 2 $IP_B
 }
 
@@ -43,7 +47,7 @@ start_netservers()
 }
 kill_netservers()
 {
-	killall -9 netserver || echo "no netservers running"
+	killall -9 netserver 2&>1 > /dev/null || echo "no netservers running"
 }
 
 banner()
@@ -68,26 +72,36 @@ basic_ratelimit_netperf(){
 	pre_test_base
     ovs-vsctl set interface $IF_B ingress_policing_rate=1000
 	ovs-vsctl set interface $IF_B ingress_policing_burst=100
-	ovs-vsctl set interface $IF_A ingress_policing_rate=1000
-	ovs-vsctl set interface $IF_A ingress_policing_burst=100
-	pre_test_netns
-	banner "A->B starting ingress_policy_rate test 1Mbps"
 
+	ovs-vsctl set interface $IF_A ingress_policing_rate=2000
+	ovs-vsctl set interface $IF_A ingress_policing_burst=200
+	pre_test_netns
+	banner "A->B starting ingress_policy_rate test (B 1Mbps, A 2Mbps)"
 	ip netns exec $NETNS_A netperf -H $IP_B -p 1111
+
+	banner "B->A starting ingress_policy_rate test (B 1Mbps, A 1Mbps)"
+	ip netns exec $NETNS_B netperf -H $IP_A -p 1111
 }
 
 htb_queue_ratelimit_netperf(){
 
 	pre_test_base
 	ovs-vsctl set Port $IF_A qos=@newqos -- \
-	 		--id=@newqos create qos type=linux-htb other-config:max-rate=1000000 queues=0=@q0 -- \
-	 		--id=@q0 create Queue other-config:min-rate=1000000 other-config:max-rate=1000000
+	 		--id=@newqos create qos type=linux-htb other-config:max-rate=2000000 queues=0=@q0 -- \
+	 		--id=@q0 create Queue other-config:min-rate=2000000 other-config:max-rate=2000000
+
+ovs-vsctl set Port $IF_B qos=@newqos -- \
+	 		--id=@newqos create qos type=linux-htb other-config:max-rate=3000000 queues=0=@q0 -- \
+	 		--id=@q0 create Queue other-config:min-rate=3000000 other-config:max-rate=3000000
 
 
 	pre_test_netns
-	banner "A->B starting HTB queue test 1Mbps"
 
+	banner "A->B starting HTB queue test (3Mbps B, 2Mbps A)"
 	ip netns exec $NETNS_A netperf -H $IP_B -p 1111
+
+	banner "B->A starting HTB queue test (3Mbps B, 2Mbps A)"
+	ip netns exec $NETNS_B netperf -H $IP_A -p 1111
 }
 
 
@@ -107,11 +121,15 @@ prerequisites() {
 
 cleanup() {
 	kill_netservers
+	ovs-vsctl del-port $QOS_BR $IF_A || true
+	ovs-vsctl del-port $QOS_BR $IF_B || true
+	ip link del $IF_A || true
+	ip link del $IF_B || true
     ip netns del $NETNS_A 2>/dev/null || echo "cleanup: $NETNS_A didn't exist"
     ip netns del $NETNS_B 2>/dev/null || echo "cleanup: $NETNS_B didn't exist"
 	ovs-vsctl del-br $QOS_BR 2>/dev/null|| echo "cleanup: $QOS_TEST_BR didn't exist"
-	ovs-vsctl --all destroy qos
-	ovs-vsctl --all destroy queue
+	ovs-vsctl --all destroy qos || true
+	ovs-vsctl --all destroy queue || true
 }
 
 pre_test_base()
