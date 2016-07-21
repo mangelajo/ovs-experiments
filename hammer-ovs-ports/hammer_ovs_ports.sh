cfa39af98cb153a7740d42e7781a16936a26b10d
Miguel Angel Ajo <miguelangel@ajo.es>
2016-07-21 09:58:40 +0000

    Speed up handling by removing stuff and assuming uid=0 and change ofport repeat checking by looking at the logs

--- hammer-ovs-ports/hammer_ovs_ports.sh
+++ hammer-ovs-ports/hammer_ovs_ports.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+_check_ofport_repeats() {
+	TAIL=/tmp/$$.out
+	tail --lines 500 /var/log/openvswitch/ovs-vswitchd.log | grep " on port " > $TAIL
+	uniq_count=$(cat $TAIL | awk '{ print $8 }' | sort | uniq | wc --lines)
+	sort_count=$(cat $TAIL | awk '{ print $8 }' | sort | wc --lines)
+
+	if [[ "_$uniq_count" != "_$sort_count" ]]; then
+		cat $TAIL | awk '{ print $8 }' | sort > "${TAIL}_sort"
+		cat "${TAIL}_sort" | uniq > "${TAIL}_uniq"
+		echo "ERROR: ofport repeats found ( ${uniq_count} == ${sort_count} )"
+		diff "${TAIL}_sort" "${TAIL}_uniq"
+		exit 1
+	else
+		echo "No ofport repeats, everything fine ( ${uniq_count} == ${sort_count} )"
+	fi
+}
+
 _add_dhcp_port() {
     # simulate dhcp port creation
     port_id=$1
     port_mac=$2
     port_name="tap${port_id:0:11}"
     ns_name="ns-$port_id"
-    sudo ip netns add $ns_name
-    sudo ovs-vsctl --timeout=120 -- --if-exists del-port $port_name -- add-port br-int $port_name -- set Interface $port_name external-ids:iface-id=$port_id external-ids:iface-status=active external-ids:attached-mac=$port_mac type=internal
-    sudo ip link set $port_name address $port_mac
-    sudo ip link set $port_name netns $ns_name
-    sudo ip netns exec $ns_name ip link set $port_name mtu 1450
-    sudo ip netns exec $ns_name ip link set $port_name up
+    ip netns add $ns_name
+    ovs-vsctl --timeout=120 -- --if-exists del-port $port_name -- add-port br-int $port_name -- set Interface $port_name external-ids:iface-id=$port_id external-ids:iface-status=active external-ids:attached-mac=$port_mac type=internal
+    ip link set $port_name address $port_mac
+    ip link set $port_name netns $ns_name
+    ip netns exec $ns_name ip link set $port_name mtu 1450
+    ip netns exec $ns_name ip link set $port_name up
 }
 
 _add_vm_port() {
@@ -21,28 +38,28 @@ _add_vm_port() {
     br_name="qbr${port_id:0:11}"
     qvo_name="qvo${port_id:0:11}"
     qvb_name="qvb${port_id:0:11}"
-    sudo brctl addbr $br_name
-    sudo brctl setfd $br_name 0
-    sudo brctl stp $br_name off
-    sudo ip link add $qvb_name type veth peer name $qvo_name
-    sudo ip link set $qvb_name up
-    sudo ip link set $qvb_name promisc on
-#    sudo ip link set $qvb_name mtu 1450
-    sudo ip link set $qvo_name up
-    sudo ip link set $qvo_name promisc on
-#    sudo ip link set $qvo_name mtu 1450
-    sudo ip link set $br_name up
-    sudo brctl addif $br_name $qvb_name
-    sudo ovs-vsctl -- --if-exists del-port $qvo_name -- add-port br-int $qvo_name -- set Interface $qvo_name external-ids:iface-id=$port_id external-ids:iface-status=active external-ids:attached-mac=$port_mac external-ids:vm-uuid=388c2414-71be-4ad4-aeeb-c74ae28d984c
-    sudo ip link set $qvo_name mtu 1450
+#    brctl addbr $br_name
+#    brctl setfd $br_name 0
+#    brctl stp $br_name off
+    ip link add $qvb_name type veth peer name $qvo_name
+#    ip link set $qvb_name up
+#    ip link set $qvb_name promisc on
+#    ip link set $qvb_name mtu 1450
+#    ip link set $qvo_name up
+#    ip link set $qvo_name promisc on
+#    ip link set $qvo_name mtu 1450
+#    ip link set $br_name up
+#    brctl addif $br_name $qvb_name
+    ovs-vsctl -- --if-exists del-port $qvo_name -- add-port br-int $qvo_name -- set Interface $qvo_name external-ids:iface-id=$port_id external-ids:iface-status=active external-ids:attached-mac=$port_mac external-ids:vm-uuid=388c2414-71be-4ad4-aeeb-c74ae28d984c
+    ip link set $qvo_name mtu 1450
 }
 
 _clean_up_dhcp_port() {
     port_id=$1
     port_name="tap${port_id:0:11}"
     ns_name="ns-$port_id"
-    sudo ovs-vsctl --if-exists del-port $port_name
-    sudo ip netns del $ns_name
+    ovs-vsctl --if-exists del-port $port_name
+    ip netns del $ns_name
 }
 
 _clean_up_vm_port() {
@@ -50,15 +67,15 @@ _clean_up_vm_port() {
     br_name="qbr${port_id:0:11}"
     qvo_name="qvo${port_id:0:11}"
     qvb_name="qvb${port_id:0:11}"
-    sudo ovs-vsctl --if-exists del-port $qvo_name
-    sudo brctl delif $br_name $qvb_name
-    sudo ip link set $br_name down
-    sudo brctl delbr $br_name
-    sudo ip link del $qvb_name
+    ovs-vsctl --if-exists del-port $qvo_name
+#    brctl delif $br_name $qvb_name
+#    ip link set $br_name down
+#    brctl delbr $br_name
+    ip link del $qvb_name
 }
 
 _find_br_ofport() {
-  sudo ovs-ofctl show $1 | grep $2 | cut -d\( -f 1 | awk '{print $1}'
+  ovs-ofctl show $1 | grep $2 | cut -d\( -f 1 | awk '{print $1}'
 }
 
 _find_vm_ofport() {
@@ -78,6 +95,8 @@ set -x
 INPUT_FILE=$1
 INPUT_FILE=${INPUT_FILE:-port_list.csv}
 IFS=,
+
+ovs-appctl vlog/set file:info
 
 while :
 do
@@ -99,32 +118,9 @@ do
 	   else
 		_add_dhcp_port $port_id $port_mac
 	   fi
+	   _check_ofport_repeats
 	done < $INPUT_FILE 
 
 
-	sleep 5
-
-	OFPORTS=""
-	while read port_id port_mac port_type
-	do
-	   if [[ "$port_type" == "VM" ]]; then
-		OFPORTS="$OFPORTS $(_find_vm_ofport $port_id)"
-	   else
-		OFPORTS="$OFPORTS $(_find_dhcp_ofport $port_id)"
-	   fi
-	done < $INPUT_FILE
-
-	sorted=$(echo $OFPORTS | tr " " "\n" | sort)
-	dedup=$(echo $OFPORTS  | tr " " "\n" | sort | uniq)
-	if [ "$sorted" = "$dedup" ]; then
-	    echo $dedup
-	else
-	    killall hammer_ovs_ports.sh ||:
-
-	    echo "Duplicate detected!"
-	    echo $sorted
-	    echo $dedup
-	    exit
-	fi
 done
 
